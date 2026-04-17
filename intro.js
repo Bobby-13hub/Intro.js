0dba7fd15a7402168763d78c8c2d3537f8e6951f
BobbyCodes <bobolaoyebajo@gmail.com>
2026-04-17 21:12:39 +0100

    updated year of study

--- intro.js
+++ intro.js
@@ -1,7 +1,9 @@
 const name = "Oyebobola";
 const age = 16;
 const favlanguage = "JavaScript";
+const study_year = 2026;
 
 console.log(`Name: ${name}`);
 console.log(`Age: ${age}`);
 console.log(`Favourite_Programming_Language: ${favlanguage}`);
+console.log(`Year Of Study: ${study_year}`);
