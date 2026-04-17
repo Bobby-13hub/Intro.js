b2691f843097d8edf9809626be50a1f8e03d7aba
BobbyCodes <bobolaoyebajo@gmail.com>
2026-04-17 20:56:03 +0100

    Update bio

---
+++ bio.js
@@ -0,0 +1,6 @@
+const state = "Ogun";
+const job = "Doctor";
+const learn = "full stack development";
+
+console.log(`I am from ${state}, I want to be a ${job},
+     and I'm excited to learn ${learn}. `);
