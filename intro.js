14522ce18d84d89387a6cfc73c31450dd74a80c9
BobbyCodes <bobolaoyebajo@gmail.com>
2026-04-17 20:29:23 +0100

    Initial commit: Add intro.js

---
+++ intro.js
@@ -0,0 +1,7 @@
+const name = "Oyebobola";
+const age = 16;
+const favlanguage = "JavaScript";
+
+console.log(`Name: ${name}`);
+console.log(`Age: ${age}`);
+console.log(`Favourite_Programming_Language: ${favlanguage}`);
